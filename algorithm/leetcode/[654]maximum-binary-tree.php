1cc85d7cad05ef90acd77b3173c40e0b5fbcc212
Morty <wangyu_95@outlook.com>
2021-03-20 15:03:58 +0800

    leetcode-654

---
+++ algorithm/leetcode/[654]maximum-binary-tree.php
@@ -0,0 +1,119 @@
+<?php
+
+//给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下： 
+//
+// 
+// 二叉树的根是数组 nums 中的最大元素。 
+// 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。 
+// 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。 
+// 
+//
+// 返回有给定数组 nums 构建的 最大二叉树 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 0 <= nums[i] <= 1000 
+// nums 中的所有整数 互不相同 
+// 
+// Related Topics 树 
+// 👍 257 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param  Integer[]  $nums
+     * @return TreeNode
+     */
+    function constructMaximumBinaryTree($nums)
+    {
+        if (empty($nums)) {
+            return null;
+        }
+        $conut = count($nums);
+        $max = PHP_INT_MIN;
+        $maxIndex = -1;
+        for ($i = 0; $i < $conut; $i++) {
+            if ($nums[$i] > $max) {
+                $max = $nums[$i];
+                $maxIndex = $i;
+            }
+        }
+        $left = $right = [];
+        for ($i = 0; $i < $conut; $i++) {
+            if ($i < $maxIndex) {
+                $left[] = $nums[$i];
+            }
+            if ($i > $maxIndex) {
+                $right[] = $nums[$i];
+            }
+        }
+
+        $root = new TreeNode($max);
+        $root->left = $this->constructMaximumBinaryTree($left);
+        $root->right = $this->constructMaximumBinaryTree($right);
+        return $root;
+    }
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($val = 0, $left = null, $right = null)
+    {
+        $this->val = $val;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
